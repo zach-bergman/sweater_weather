ebcebe863e6c9208bd8307c62ec045f68853b39b
Zach Bergman <bzach634@gmail.com>
2024-08-05 15:54:59 -0600

    test: Add sad path tests and add to User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  before :each do
+    @user = User.create!(email: "user@email.com", password: "password", password_confirmation: "password")
+  end
+
   describe "validations" do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of(:email).case_insensitive }
@@ -11,5 +15,13 @@ RSpec.describe User, type: :model do
 
     it { should have_secure_token :api_key }
     it { should validate_uniqueness_of :api_key }
+  end
+
+  it "should have a secure password and secure api_key" do
+    expect(@user.authenticate("password")).to eq(@user)
+    expect(@user).to_not have_attribute(:password)
+    expect(@user.password_digest).to_not eq("password")
+    expect(@user.api_key).to_not eq(nil)
+    expect(@user.api_key).to_not eq("")
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -11,10 +11,153 @@ RSpec.describe "Users" do
 
       headers = {"CONTENT_TYPE" => "application/json"}
 
-      post "/api/v1/users", headers: headers, params: JSON.generate(user: user_params)
+      post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
 
       created_user = User.last
-      binding.pry
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body).to be_a(Hash)
+      expect(response_body).to have_key(:data)
+      expect(response_body[:data]).to be_a(Hash)
+      expect(response_body[:data]).to have_key(:id)
+      expect(response_body[:data][:id]).to be_a(String)
+      expect(response_body[:data]).to have_key(:type)
+      expect(response_body[:data][:type]).to eq("users")
+      expect(response_body[:data]).to have_key(:attributes)
+      expect(response_body[:data][:attributes]).to be_a(Hash)
+      expect(response_body[:data][:attributes]).to have_key(:email)
+      expect(response_body[:data][:attributes][:email]).to be_a(String)
+      expect(response_body[:data][:attributes]).to have_key(:api_key)
+      expect(response_body[:data][:attributes][:api_key]).to be_a(String)
+      expect(response_body[:data][:attributes][:api_key]).to eq(created_user.api_key)
+      
+      # it does not return the password
+      expect(response_body[:data]).to_not have_key(:password)
+      expect(response_body[:data][:attributes]).to_not have_key(:password)
+    end
+
+    describe "sad path" do
+      it "returns an error if email is not present or is taken" do
+        user_params = {
+          email: "",
+          password: "password",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+    
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+  
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:errors]).to be_a(Array)
+        expect(response_body[:errors].first[:status]).to eq(400)
+        expect(response_body[:errors].first[:message]).to eq("Email can't be blank")
+      end
+
+      it "returns an error if email is not unique" do
+        user_params = {
+          email: "user_1@email.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+
+        user_params = {
+          email: "user_1@email.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+  
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:errors]).to be_a(Array)
+        expect(response_body[:errors].first[:status]).to eq(400)
+        expect(response_body[:errors].first[:message]).to eq("Email has already been taken")
+      end
+
+      it "returns an error if password is not present" do
+        user_params = {
+          email: "user_1@email.com",
+          password: "",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+    
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+  
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:errors]).to be_a(Array)
+        expect(response_body[:errors].first[:status]).to eq(400)
+        expect(response_body[:errors].first[:message]).to eq("Password can't be blank, Password can't be blank, and Password is too short (minimum is 6 characters)")
+      end
+
+      it "returns an error if password is not longer than 6 characters" do
+        user_params = {
+          email: "user_1@email.com",
+          password: "hi",
+          password_confirmation: "hi"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+    
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+  
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:errors]).to be_a(Array)
+        expect(response_body[:errors].first[:status]).to eq(400)
+        expect(response_body[:errors].first[:message]).to eq("Password is too short (minimum is 6 characters)")
+      end
+      
+      it "returns an error if passwords don't match" do
+        user_params = {
+          email: "user_1@email.com",
+          password: "password",
+          password_confirmation: "hi"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+    
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+  
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:errors]).to be_a(Array)
+        expect(response_body[:errors].first[:status]).to eq(400)
+        expect(response_body[:errors].first[:message]).to eq("Password confirmation doesn't match Password")
+      end
     end
   end
 end
