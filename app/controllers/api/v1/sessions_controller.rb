1eea18290232a8a1fcaabe075fa348eab9386779
Zach Bergman <bzach634@gmail.com>
2024-08-05 17:46:13 -0600

    feat: Add SessionsController with create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      render json: UsersSerializer.new(user), status: :ok
+    else
+      render json: ErrorSerializer.new(ErrorMessage.new("Invalid credentials", 401)).serialize_json, status: 401
+    end
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
+  end
+end
