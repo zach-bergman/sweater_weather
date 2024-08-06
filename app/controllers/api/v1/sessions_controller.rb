717b55b478cee34357a28af97f20352bfd0e999f
Zach Bergman <bzach634@gmail.com>
2024-08-05 18:41:11 -0600

    feat: Add new mapquest endpoint in service

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
     if user && user.authenticate(session_params[:password])
+      # session cookie?
       render json: UsersSerializer.new(user), status: :ok
     else
       render json: ErrorSerializer.new(ErrorMessage.new("Invalid credentials", 401)).serialize_json, status: 401
